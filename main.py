20e05c4353e5886e0b772409a31a4ae8899c0c2d
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-09-23 19:51:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,5 @@ import math
 x=int(input("Введите x: "))
 y=int(input("Введите y: "))
 z=int(input("Введите z: "))
-#n=x**(1/3)
-#p=x**(y+2)
-#m=abs(x-y)
-#s=(math.asin(z))**2
-#b=(10*(n+p))**(1/2)*(s-m)
 b=((10*((x**(1/3))+(x**(y+2))))**(1/2))*((math.asin(z))**2-abs(x-y))
 print(b)
