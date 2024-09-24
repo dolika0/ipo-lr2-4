c780489462e89e14f10e50b192139144f0c429df
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-09-24 11:21:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ import math
 x=int(input("Введите x: "))
 y=int(input("Введите y: "))
 z=int(input("Введите z: "))
-b=((10*((x**(1/3))+(x**(y+2))))**(1/2))*((math.asin(z))**2-abs(x-y))
-print(b)
+if z<-1 or z>1 :
+  print("Ошибка!z может быть от [-1;1] . ")
+else :
+  b=((10*((x**(1/3))+(x**(y+2))))**(1/2))*((math.asin(z))**2-abs(x-y))
+  print(b)
